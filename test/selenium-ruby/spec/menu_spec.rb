fa547039f9034b0d28bf22d200f057005595f8b3
Elizabeth Tatum <bethtatum@gmail.com>
2015-06-19 11:10:06 -0400

    acceptance test updates

--- test/selenium-ruby/spec/menu_spec.rb
+++ test/selenium-ruby/spec/menu_spec.rb
@@ -4,29 +4,32 @@ describe 'Menu' do
   # renaming to _draft - these tests are done in other scripts. no need for it anymore
   before(:all) do
     @splash = Splash.new (@driver)
-
-
-    expect(@driver.find_element(:id, "proceed-to-app-btn").text).to eq "Proceed to App"
-
+    @driver.find_element(:id, "proceed-to-app-btn").click
   end
 
 
   it 'will have the correct elements' do
     @driver.find_element(:class, "menu-toggle").click
-    element_present?(:link, "Med List").should be_true
-    element_present?(:link, "Graph").should be_true
-    # any others
+    expect(element_present?(:link, "Med List")).to be_truthy
+    expect(element_present?(:link, "Graph")).to be_truthy
+    expect(element_present?(:link, "About")).to be_truthy
+    @driver.find_element(:class, "menu-toggle").click
   end
 
-  it 'clicking med list will take you to the correct page' do
+  it 'clicking med list link will take you to the correct page' do
     @driver.find_element(:class, "menu-toggle").click
     @driver.find_element(:link, "Med List").click
     # verify on the med list page
   end
-  it 'clicking med list will take you to the correct page' do
+  it 'clicking graph link will take you to the correct page' do
     @driver.find_element(:class, "menu-toggle").click
     @driver.find_element(:link, "Graph").click
     # verify on the graph page
+  end
+  it 'clicking about will take you to the correct page' do
+    @driver.find_element(:class, "menu-toggle").click
+    @driver.find_element(:link, "About").click
+    # verify on the about page
   end
 
 end
