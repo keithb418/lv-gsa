de37cf56c3e60e947cae56c00d567cdb1e011cc6
Keith Brown <kbrown@longviewinc.com>
2015-06-24 14:45:08 -0400

    Fixed the medSearchItem spec

--- test/unit_tests/specs/medSearchItemSpec.js
+++ test/unit_tests/specs/medSearchItemSpec.js
@@ -16,18 +16,42 @@ define(function (require) {
 	};
 
 	describe('Med Search Item', function () {
-		it('Adds an item to the med list', function () {
+		beforeEach(function () {
+			spyOn(App.vent, 'trigger');
+			App.collections.medList.reset();
+		});
+		
+		it('adds an item to the med list', function () {
 			expect(App.collections.medList.get('abc')).toBeFalsy();
 			
 			medSearchItem.addItemToList();
 			expect(App.collections.medList.get('abc')).toBeTruthy();
+			expect(App.vent.trigger).toHaveBeenCalledWith('refresh:med:list');
+			expect(App.vent.trigger).toHaveBeenCalledWith('sync:local:storage');
 		});
 		
-		it('closes the search results', function () {
-			spyOn(App.vent, 'trigger');
+		it('does not add an item to the med list if the list has ten or more items', function () {
+			for (var i = 0; i < 10; i++) {
+				App.collections.medList.push(new Backbone.Model({id: i}));
+			}
+			
+			expect(App.collections.medList.length).toEqual(10);
 			
 			medSearchItem.addItemToList();
-			expect(App.vent.trigger).toHaveBeenCalledWith('clear:search');
+			expect(App.collections.medList.length).toEqual(10);
+			expect(App.collections.medList.get('abc')).toBeFalsy();
+			expect(App.vent.trigger).not.toHaveBeenCalledWith('refresh:med:list');
+			expect(App.vent.trigger).not.toHaveBeenCalledWith('sync:local:storage');
+		});
+		
+		it('udpates the search', function () {
+			medSearchItem.addItemToList();
+			expect(App.vent.trigger).toHaveBeenCalledWith('update:search');
+		});
+		
+		it('shows/hides the action button', function () {
+			medSearchItem.addItemToList();
+			expect(App.vent.trigger).toHaveBeenCalledWith('show:hide:action');
 		});
 	});
 });
