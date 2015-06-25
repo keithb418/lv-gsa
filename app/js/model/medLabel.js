85a45c56c013dad217818e82d638882ddd21a00c
Stephen Wright <swright@longviewinc.com>
2015-06-25 15:31:03 -0400

    fixing hint errors

--- app/js/model/medLabel.js
+++ app/js/model/medLabel.js
@@ -5,14 +5,14 @@ define(function (require) {
   return Backbone.Model.extend({
     defaults: {
       adverse_reactions: '',            
-  	  description: '',
-  	  warnings: '',
-  	  warnings_and_cautions: '',
-  	  openfda: {
-  	    brand_name: '',
-  	    generic_name: '',
-  	    manufacturer_name: ''
-  	  }
+      description: '',
+      warnings: '',
+      warnings_and_cautions: '',
+      openfda: {
+        brand_name: '',
+        generic_name: '',
+        manufacturer_name: ''
+      }
     }
   });
 });
